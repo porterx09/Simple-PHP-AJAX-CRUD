368e9aeda02f97fba240840d081264ee427fa6e5
Albert <alberto.galimba@gmail.com>
2019-03-01 12:16:08 +0800

    Db/SQL

---
+++ system/database/db.php
@@ -0,0 +1,46 @@
+<?php
+
+class Db {
+  
+  protected $host = 'localhost';
+  protected $user = 'root';
+  protected $pass = '';
+  protected $dbname = 'php_ajax_crud';
+  protected $con;
+  
+  public function __construct()
+  {
+    $this->connect();
+  }
+  
+  protected function connect()
+  {
+    $this->con = mysqli_connect($this->host, $this->user, $this->pass, $this->dbname);
+    if(!$this->con)
+    {
+      header('Location: ../../page/error/503.html');
+    }
+  }
+  
+  public function query($query) 
+  {
+    return mysqli_query($this->con, $query);
+  }
+  
+  public function fetch($res) 
+  {
+    return mysqli_fetch_assoc($res);
+  }
+  
+  public function insert_id()
+  {
+    return mysqli_insert_id($this->con);
+  }
+  
+  public function sanitize($data) 
+  {
+    $data = mysqli_real_escape_string($this->con, $data);
+    $data = stripslashes($data);
+    return $data;
+  }
+}
---
+++ system/database/php_ajax_crud.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 01, 2019 at 05:14 AM
+-- Server version: 10.1.32-MariaDB
+-- PHP Version: 7.2.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `php_ajax_crud`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employees`
+--
+
+CREATE TABLE `employees` (
+  `employee_id` int(11) NOT NULL,
+  `firstname` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `middlename` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `lastname` varchar(50) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `employees`
+--
+
+INSERT INTO `employees` (`employee_id`, `firstname`, `middlename`, `lastname`) VALUES
+(35, 'Alberto Jr.', 'Galimba', 'Arellano');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `employees`
+--
+ALTER TABLE `employees`
+  ADD PRIMARY KEY (`employee_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `employees`
+--
+ALTER TABLE `employees`
+  MODIFY `employee_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
